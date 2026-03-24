a1a72f2d9e57224727c3a344ad464d7c7f175f57
Tuoyoesimaje <esimajetuoyo71@gmail.com>
2026-03-24 14:50:12 +0100

    Add favicon to build script and HTML files

--- build.sh
+++ build.sh
@@ -11,6 +11,7 @@ cp policies.html public/
 cp logo.jpg public/
 cp sitemap.xml public/
 cp robots.txt public/
+cp favicon-32x32.png public/
 cp "Generated Image January 09, 2026 - 11_19AM.jpeg" public/ 2>/dev/null || true
 
 # Build React app
